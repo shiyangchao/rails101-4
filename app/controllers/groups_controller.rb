a4c354a40dc07c955e6a3bd1ee1a3f1cbf79b7fb
chaoshiyang <hao4421444469@gmail.com>
2017-01-12 23:08:03 +0800

    use simple_format helper to decorate

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
-  def index
-    @groups = current_user.participated_groups
-  end
 
   def show
     @group = Group.find(params[:id])
